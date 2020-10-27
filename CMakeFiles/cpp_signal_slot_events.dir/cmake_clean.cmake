440d0f7186068888e4f523e261440bc43b0fa80e
masoud-iranmehr <masoud.iranmehr@gmail.com>
2020-10-26 23:22:51 -0400

    first commit

---
+++ CMakeFiles/cpp_signal_slot_events.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_signal_slot_events.dir/main.cpp.o"
+  "cpp_signal_slot_events.pdb"
+  "cpp_signal_slot_events"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_signal_slot_events.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
